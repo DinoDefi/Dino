1ab9797ac17f2444c2563207062f186f0e9d06ea
Dino <83650026+DinoDefi@users.noreply.github.com>
2021-06-24 13:43:37 +0800

    Update DinoToken.sol

--- DinoToken.sol
+++ DinoToken.sol
@@ -258,8 +258,8 @@ contract ERC20 is Context, IERC20, Ownable {
 
 
 
-contract DinoToken is ERC20('Dino', 'DINO') {
-    /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
+contract DinoToken is ERC20('DinoDefi', 'DINO') {
+    /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChief).
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
         _moveDelegates(address(0), _delegates[_to], _amount);
